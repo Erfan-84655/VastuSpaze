b8e08a31689b22c7915d0fae22a5cfaa3967b8a5
Erfan <erfanfallah285@gmail.com>
2026-07-03 21:12:38 +0330

    finish the ContactUs section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {About, Hero, Navbar,Portfolio,Reviews,Services} from '../src/Components/index'
+import {About, ContactUs, Hero, Navbar,Portfolio,Reviews,Services} from '../src/Components/index'
 export default function App() {
   return (
   <main className='overflow-x-hidden antialiased text-neutral-800'>
@@ -9,6 +9,7 @@ export default function App() {
     <Portfolio/>
     <About/>
     <Reviews/>
+    <ContactUs/>
   </main>
   )
 }
--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -5,4 +5,5 @@ import Services from "./Services";
 import Portfolio from './Portfolio'
 import About from "./About";
 import Reviews from './Reviews'
-export {Navbar,Footer,Hero,Services,Portfolio,About,Reviews}
+import ContactUs from "./ContactUs";
+export {Navbar,Footer,Hero,Services,Portfolio,About,Reviews,ContactUs}
