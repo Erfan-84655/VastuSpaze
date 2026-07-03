ba818a74796f7039637e3fba91d52ee4bd7f827f
Erfan <erfanfallah285@gmail.com>
2026-07-03 22:03:58 +0330

    finish the footer section

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { RiFacebookFill, RiInstagramFill, RiTwitterFill } from "react-icons/ri";
 
 export default function Footer() {
   return (
-    <div>
-      
-    </div>
-  )
+    <footer className="max-w-7xl mx-auto">
+      <div className="flex flex-col md:flex-row justify-between items-center border-t-2 py-4">
+        <div className="flex space-x-6 mb-2">
+          <a
+            href="https://www.facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="visit our facebook page"
+          >
+            <RiFacebookFill />
+          </a>
+          <a
+            href="https://www.twitter.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="visit our Twitter page"
+          >
+            <RiTwitterFill />
+          </a>
+          <a
+            href="https://www.instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="visit our Instagram page"
+          >
+            <RiInstagramFill />
+          </a>
+        </div>
+        <p className="text-sm ">&copy; 2026 VastuSpaze. All rights reserved.</p>
+        <p className="text-sm ">Created by Erfan Fallah</p>
+      </div>
+    </footer>
+  );
 }
