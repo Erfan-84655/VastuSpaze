945071bf26a781c9b991f22464aa1e8fb6f0aa47
Erfan <erfanfallah285@gmail.com>
2026-07-03 20:26:13 +0330

    finish About Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Hero, Navbar,Portfolio,Services} from '../src/Components/index'
+import {About, Hero, Navbar,Portfolio,Services} from '../src/Components/index'
 export default function App() {
   return (
   <main className='overflow-x-hidden antialiased text-neutral-800'>
@@ -7,6 +7,7 @@ export default function App() {
     <Hero/>
     <Services/>
     <Portfolio/>
+    <About/>
   </main>
   )
 }
--- src/Components/About/index.jsx
+++ src/Components/About/index.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
-
+import React from "react";
+import aboutImage from "../../assets/about.webp";
 export default function About() {
   return (
-    <div>
-      
-    </div>
-  )
+    <section className="max-w-7xl mx-auto border-b-2 mt-20 " id="about">
+      <h2 className="text-xl lg:text-3xl tracking-tight text-center uppercase mb-12 mx-4">
+        About
+      </h2>
+      <div className="flex flex-col items-center lg:space-x-8 mx-4 mb-20">
+        <div className="mb-8 lg:mb-0">
+          <img
+            src={aboutImage}
+            className="w-full h-auto"
+            alt="About VastuSpaze"
+          />
+        </div>
+        <p className="text-lg lg:text-3xl font-light text-center lg:text-left max-w-5xl mx-auto mt-8">
+          At VastuSpgze, we believe in transforming houses into dream homes. I
+          with a passion for design and a commitment to quality, our team of
+          skilled professionals has been dedicated to delivering exceptional
+          renovation services for over a decade. We specialize in creating
+          beautiful, functional spaces that reflect our clients unique tastes
+          and lifestyles. From modern kitchens and luxurious bathrooms to serene
+          outdoor living areas and cozy home offices, we bring our expertise to
+          every project, ensuring each renovation is tailored to meet vour
+          specific needs and desires. Our mission is to enhance the beauty and
+          functionality of your home, making it a place where you can truly
+          thrive.
+        </p>
+      </div>
+    </section>
+  );
 }
