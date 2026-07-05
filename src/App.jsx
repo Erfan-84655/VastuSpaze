3f3e69c87fdfb6f97d458cf00180f5ac76c3a5d6
Erfan <erfanfallah285@gmail.com>
2026-07-05 19:01:37 +0330

    refactor: format code for consistency and readability

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import {About, ContactUs, Footer, Hero, Navbar,Portfolio,Reviews,Services} from '../src/Components/index'
+import {
+  About,
+  ContactUs,
+  Footer,
+  Hero,
+  Navbar,
+  Portfolio,
+  Reviews,
+  Services,
+} from "../src/Components/index";
 export default function App() {
   return (
-  <main className='overflow-x-hidden antialiased text-neutral-800'>
-    <Navbar/>
-    <Hero/>
-    <Services/>
-    <Portfolio/>
-    <About/>
-    <Reviews/>
-    <ContactUs/>
-    <Footer/>
-  </main>
-  )
+    <main className="overflow-x-hidden antialiased text-neutral-800">
+      <Navbar />
+      <Hero />
+      <Services />
+      <Portfolio />
+      <About />
+      <Reviews />
+      <ContactUs />
+      <Footer />
+    </main>
+  );
 }
